101904e19a5a459490079a3252bd8cf49675164c
Sebastián Andrés Huérfano Rodríguez <xsebastianhrodriguezx@gmail.com>
2019-12-18 14:13:54 -0500

    Se suben los cambios de diseño del modulo de admin.

--- resources/views/layouts-admin/app.blade.php
+++ resources/views/layouts-admin/app.blade.php
@@ -185,7 +185,7 @@
                     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                         <!-- Sidebar Toggle (Topbar) -->
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <button id="sidebarToggleTop" class="btn btn-link d-block rounded-circle mr-3">
                             <i class="fa fa-bars"></i>
                         </button>
 
@@ -360,8 +360,7 @@
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}
-                                        <br> ( {{ Auth::user()->Company_id }} ) </span>
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                                     <img class="img-profile rounded-circle"
                                         src="{{ asset('img/profile_default.png') }}">
                                 </a>
