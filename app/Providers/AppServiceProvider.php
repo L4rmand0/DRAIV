a0aa0b6ea78ee07dad1de8d22fce30d6efcdacf9
Sebastián Huérfano <xsebastianhrodriguezx@gmail.com>
2020-01-14 16:24:30 -0500

    Se sube la carpeta init.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    use Schema;
     /**
      * Register any application services.
      *
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
