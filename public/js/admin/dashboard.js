a639b10650119ed4f16f3442310469f7598767e2
Sebastián Huérfano <xsebastianhrodriguezx@gmail.com>
2020-01-28 09:28:05 -0500

    Se suben los cambios de la tabla sumarize.

--- public/js/admin/dashboard.js
+++ public/js/admin/dashboard.js
@@ -1,5 +1,5 @@
 // IIFE - Immediately Invoked Function Expression
-var $select2_drivers
+var table_sumarize;
 (function (runcode) {
 
     // The global jQuery object is passed as a parameter
@@ -32,6 +32,7 @@ var $select2_drivers
         var chart_line_v;
         var chart_brand_v;
         var chart_model_v;
+        var $select2_driversvar; 
         var chart_drivers_v;
 
 
@@ -60,6 +61,7 @@ var $select2_drivers
         $("#select_company_dash").on("change", function () {
             $target = $(this);
             $("#dashboard_company_id").val($target.val());
+            $("#span_name_company").text($("#select_company_dash option:selected").text());
             $.ajax({
                 type: 'GET',
                 url: $('#select_cc_driver').data('url'),
@@ -75,6 +77,43 @@ var $select2_drivers
                     }
                 });
             });
+
+            dataSumarize().done(function (response) {
+                table_sumarize.clear().draw();
+                table_sumarize.rows.add(response.data);
+                table_sumarize.columns.adjust().draw();
+            //     table_sumarize = $('#table_sumarize').DataTable({
+            //        destroy: true,
+            //        data: response.data,
+            //        scrollX: true,
+            //        columns: [
+            //            { data: 'first_name', name: 'first_name'},
+            //            { data: 'second_name', name: 'second_name'},
+            //            { data: 's_last_name', name: 's_last_name'},
+            //            { data: 'f_last_name', name: 'f_last_name'},
+            //            { data: 'licencia', name: 'licencia'},
+            //            { data: 'categoría', name: 'categoría'},
+            //            { data: 'fecha_licencia_expedicion', name: 'fecha_licencia_expedicion'},
+            //            { data: 'fecha_licencia_vencimiento', name: 'fecha_licencia_vencimiento'},
+            //            { data: 'pais_licencia_expedicion', name: 'pais_licencia_expedicion'},
+            //            { data: 'placa', name: 'placa'},
+            //            { data: 'tipo_vehiculo', name: 'tipo_vehiculo'},
+            //            { data: 'propietario', name: 'propietario'},
+            //            { data: 'tipo_taxi', name: 'tipo_taxi'},
+            //            { data: 'numero_conductores', name: 'numero_conductores'},
+            //            { data: 'fecha_vencimiento_soat', name: 'fecha_vencimiento_soat'},
+            //            { data: 'capacidad', name: 'capacidad'},
+            //            { data: 'servicio', name: 'servicio'},
+            //            { data: 'cilindraje', name: 'cilindraje'},
+            //            { data: 'modelo', name: 'modelo'},
+            //            { data: 'linea', name: 'linea'},
+            //            { data: 'marca', name: 'marca'},
+            //            { data: 'color', name: 'color'},
+            //            { data: 'fecha_tecnomecanica', name: 'fecha_tecnomecanica'},
+            //        ],
+            //        language: language_dt,
+            //    });
+           });
         });
 
         $("#select_cc_driver").on('change', function () {
@@ -593,43 +632,77 @@ var $select2_drivers
             }
         });
 
-        $.ajax({
-            type: 'GET',
-            url: $('#table_sumarize').data('url'),
-            data: { 'type': 'select_admin2', 'company_id': $("#dashboard_company_id").val() },
-        }).done(function (response) {
-            var table_sumarize = $('#table_sumarize').DataTable({
-                destroy: true,
-                data: response.data,
-                scrollX: true,
-                columns: [
-                    { data: 'first_name', name: 'first_name'},
-                    { data: 'second_name', name: 'second_name'},
-                    { data: 's_last_name', name: 's_last_name'},
-                    { data: 'f_last_name', name: 'f_last_name'},
-                    { data: 'licencia', name: 'licencia'},
-                    { data: 'categoría', name: 'categoría'},
-                    { data: 'fecha_licencia_expedicion', name: 'fecha_licencia_expedicion'},
-                    { data: 'fecha_licencia_vencimiento', name: 'fecha_licencia_vencimiento'},
-                    { data: 'pais_licencia_expedicion', name: 'pais_licencia_expedicion'},
-                    { data: 'placa', name: 'placa'},
-                    { data: 'tipo_vehiculo', name: 'tipo_vehiculo'},
-                    { data: 'propietario', name: 'propietario'},
-                    { data: 'tipo_taxi', name: 'tipo_taxi'},
-                    { data: 'numero_conductores', name: 'numero_conductores'},
-                    { data: 'fecha_vencimiento_soat', name: 'fecha_vencimiento_soat'},
-                    { data: 'capacidad', name: 'capacidad'},
-                    { data: 'servicio', name: 'servicio'},
-                    { data: 'cilindraje', name: 'cilindraje'},
-                    { data: 'modelo', name: 'modelo'},
-                    { data: 'linea', name: 'linea'},
-                    { data: 'marca', name: 'marca'},
-                    { data: 'color', name: 'color'},
-                    { data: 'fecha_tecnomecanica', name: 'fecha_tecnomecanica'},
-                ],
-                language: language_dt,
-            });
-        });
+        // $.ajax({
+        //     type: 'GET',
+        //     url: $('#table_sumarize').data('url'),
+        //     data: { 'type': 'select_admin2', 'company_id': $("#dashboard_company_id").val() },
+        // }).done(function (response) {
+        //     table_sumarize = $('#table_sumarize').DataTable({
+        //         destroy: true,
+        //         data: response.data,
+        //         scrollX: true,
+        //         columns: [
+        //             { data: 'first_name', name: 'first_name'},
+        //             { data: 'second_name', name: 'second_name'},
+        //             { data: 's_last_name', name: 's_last_name'},
+        //             { data: 'f_last_name', name: 'f_last_name'},
+        //             { data: 'licencia', name: 'licencia'},
+        //             { data: 'categoría', name: 'categoría'},
+        //             { data: 'fecha_licencia_expedicion', name: 'fecha_licencia_expedicion'},
+        //             { data: 'fecha_licencia_vencimiento', name: 'fecha_licencia_vencimiento'},
+        //             { data: 'pais_licencia_expedicion', name: 'pais_licencia_expedicion'},
+        //             { data: 'placa', name: 'placa'},
+        //             { data: 'tipo_vehiculo', name: 'tipo_vehiculo'},
+        //             { data: 'propietario', name: 'propietario'},
+        //             { data: 'tipo_taxi', name: 'tipo_taxi'},
+        //             { data: 'numero_conductores', name: 'numero_conductores'},
+        //             { data: 'fecha_vencimiento_soat', name: 'fecha_vencimiento_soat'},
+        //             { data: 'capacidad', name: 'capacidad'},
+        //             { data: 'servicio', name: 'servicio'},
+        //             { data: 'cilindraje', name: 'cilindraje'},
+        //             { data: 'modelo', name: 'modelo'},
+        //             { data: 'linea', name: 'linea'},
+        //             { data: 'marca', name: 'marca'},
+        //             { data: 'color', name: 'color'},
+        //             { data: 'fecha_tecnomecanica', name: 'fecha_tecnomecanica'},
+        //         ],
+        //         language: language_dt,
+        //     });
+        // });
+
+        dataSumarize().done(function (response) {
+            table_sumarize = $('#table_sumarize').DataTable({
+               destroy: true,
+               data: response.data,
+               scrollX: true,
+               columns: [
+                   { data: 'first_name', name: 'first_name'},
+                   { data: 'second_name', name: 'second_name'},
+                   { data: 's_last_name', name: 's_last_name'},
+                   { data: 'f_last_name', name: 'f_last_name'},
+                   { data: 'licencia', name: 'licencia'},
+                   { data: 'categoría', name: 'categoría'},
+                   { data: 'fecha_licencia_expedicion', name: 'fecha_licencia_expedicion'},
+                   { data: 'fecha_licencia_vencimiento', name: 'fecha_licencia_vencimiento'},
+                   { data: 'pais_licencia_expedicion', name: 'pais_licencia_expedicion'},
+                   { data: 'placa', name: 'placa'},
+                   { data: 'tipo_vehiculo', name: 'tipo_vehiculo'},
+                   { data: 'propietario', name: 'propietario'},
+                   { data: 'tipo_taxi', name: 'tipo_taxi'},
+                   { data: 'numero_conductores', name: 'numero_conductores'},
+                   { data: 'fecha_vencimiento_soat', name: 'fecha_vencimiento_soat'},
+                   { data: 'capacidad', name: 'capacidad'},
+                   { data: 'servicio', name: 'servicio'},
+                   { data: 'cilindraje', name: 'cilindraje'},
+                   { data: 'modelo', name: 'modelo'},
+                   { data: 'linea', name: 'linea'},
+                   { data: 'marca', name: 'marca'},
+                   { data: 'color', name: 'color'},
+                   { data: 'fecha_tecnomecanica', name: 'fecha_tecnomecanica'},
+               ],
+               language: language_dt,
+           });
+       });
 
     });
 
@@ -1108,6 +1181,15 @@ var $select2_drivers
                     $("#card_technomecanical_expirated_number").text(datac.response);
                 }
             }
+        });
+    }
+
+
+    function dataSumarize(){
+       return $.ajax({
+            type: 'GET',
+            url: $('#table_sumarize').data('url'),
+            data: { 'type': 'select_admin2', 'company_id': $("#dashboard_company_id").val() },
         });
     }
 
